480a264e4210cb359b53bf8efd73cc76de132f1b
Stanislas Gueniffey <sgueniff@ulb.ac.be>
2017-11-18 14:35:14 +0100

    Compute RIPEMD160 digest of a public key to obtain address
    Use BouncyCastle's implementation of the RIPEMD160 algorithm, add dependency to the pom.xml.

--- src/be/ac/ulb/crashcoin/data/Address.java
+++ src/be/ac/ulb/crashcoin/data/Address.java
@@ -1,5 +1,7 @@
 package be.ac.ulb.crashcoin.data;
 
+import org.bouncycastle.crypto.digests.RIPEMD160Digest;
+
 import java.security.PublicKey;
 
 public class Address {
@@ -16,13 +18,15 @@ public class Address {
      * Apply RIPEMD160 algorithm to retrieve the CrashCoin address from the public
      * key.
      * 
-     * @param key
-     *            Public key
+     * @param key Public key
      * @return Byte representation of the CrashCoin address
      */
     private byte[] applyRIPEMD160(PublicKey key) {
         byte[] bytes = key.getEncoded();
-        return null; // TODO: implement algorithm and run it on bytes
+        RIPEMD160Digest d = new RIPEMD160Digest();
+        d.update(bytes, 0, bytes.length); // Copute RIPEMD160 digest
+        d.doFinal(bytes, 0); // Copy digest into bytes
+        return bytes;
     }
 
     /** Byte representation of the CrashCoin address */
