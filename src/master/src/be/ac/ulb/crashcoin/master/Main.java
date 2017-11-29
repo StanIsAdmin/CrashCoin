6afd411efd8990346369df2b5d617de1d7f724b1
denishoornaert <dhoornae@ulb.ac.be>
2017-11-29 19:08:19 +0100

    Blockchain now is instancied in the master node

--- src/master/src/be/ac/ulb/crashcoin/master/Main.java
+++ src/master/src/be/ac/ulb/crashcoin/master/Main.java
@@ -14,6 +14,7 @@ public class Main {
     private static BlockChain blockChain;
     
     public static void main(final String[] args) {
+        blockChain = new BlockChain();
         
         // Init listener
         try {
