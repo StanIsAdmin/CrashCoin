205b2999029a06798eb6b527852642eab73e34b4
Stanislas Gueniffey <sgueniff@ulb.ac.be>
2017-11-30 17:09:46 +0100

    Read BlockChain from file (if it exists) when loading master

--- src/master/src/be/ac/ulb/crashcoin/master/Main.java
+++ src/master/src/be/ac/ulb/crashcoin/master/Main.java
@@ -2,9 +2,14 @@ package be.ac.ulb.crashcoin.master;
 
 import be.ac.ulb.crashcoin.common.BlockChain;
 import be.ac.ulb.crashcoin.master.net.RelayListener;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.json.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * 
@@ -13,8 +18,10 @@ public class Main {
     
     private static BlockChain blockChain;
     
+    private static final String BLOCKCHAIN_SAVE_PATH = "./master/blockchain.json";
+    
     public static void main(final String[] args) {
-        blockChain = new BlockChain();
+        blockChain = createBlockChain();
         
         // Init listener
         try {
@@ -26,6 +33,27 @@ public class Main {
     
     public static BlockChain getBlockChain() {
         return blockChain;
+    }
+    
+    private static BlockChain createBlockChain() {
+        FileReader fr;
+        try {
+            fr = new FileReader(BLOCKCHAIN_SAVE_PATH);
+        } catch (FileNotFoundException ex) {
+            // Returns a new BlockChain if none has been saved
+            return new BlockChain();
+        }
+        
+        JSONParser parser = new JSONParser();
+        JSONObject jsonBlockChain;
+        try {
+            jsonBlockChain = (JSONObject) parser.parse(fr);
+        } catch (ParseException | IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        
+        return new BlockChain(jsonBlockChain);
     }
     
 }
