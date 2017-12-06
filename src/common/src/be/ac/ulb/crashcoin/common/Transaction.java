775d24fb9a9188a3a1988d77b73a01d2441a93eb
Stanislas Gueniffey <sgueniff@ulb.ac.be>
2017-12-06 20:49:51 +0100

    Verify transaction values in BlockChain.isValidTransaction() Call Transaction.isValid() method, make it public

--- src/common/src/be/ac/ulb/crashcoin/common/Transaction.java
+++ src/common/src/be/ac/ulb/crashcoin/common/Transaction.java
@@ -106,10 +106,19 @@ public class Transaction implements JSONable {
         this.outputs.add(new Output(address, nCrashCoins));
     }
 
-    private boolean isValid() {
+    /** Returns true if the standalone transaction is valid, false otherwise.
+     * A transaction by itself is valid if it meets all of these conditions :
+     * - the sum of inputs equals the sum of outputs
+     * - each output value is strictly positive
+     * - TODO use input values instead of totalAmount ?
+     * 
+     * @return true if the transaction is valid as described, false otherwise
+     */
+    public boolean isValid() {
         // Check whether sum of inputs is lower than the sum of outputs
         Integer sum = 0;
         for (Output output : this.outputs) {
+            if (output.nCrashCoins <= 0) return false;
             sum += output.nCrashCoins;
         }
         // The difference is considered as transaction fee
