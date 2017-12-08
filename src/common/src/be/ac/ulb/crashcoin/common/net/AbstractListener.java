091923499644813acb40414a7b3838b851823b91
Alexis Reynouard <areynoua@ulb.ac.be>
2017-12-08 10:19:49 +0100

    Comments on AbstractListener

--- src/common/src/be/ac/ulb/crashcoin/common/net/AbstractListener.java
+++ src/common/src/be/ac/ulb/crashcoin/common/net/AbstractListener.java
@@ -7,6 +7,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
+ * Manages a ServerSocket (i.e.: it is a Thread waiting for connections to accept)
+ *
+ * Subclasses should overwrite the createNewConnection method which is called
+ * each time a new connection is accepted.
  *
  * @author Detobel
  */
@@ -14,11 +18,22 @@ public abstract class AbstractListener extends Thread {
 
     protected ServerSocket _sock;
 
+    /**
+     * Create a new AbstractListener, that is a thread and a server socket.
+     *
+     * @param name the thread name (For identification purposes. More than one thread may have the same name.)
+     * @param sock the server socket
+     */
     protected AbstractListener(final String name, final ServerSocket sock) {
         super(name);
         _sock = sock;
     }
 
+    /**
+     * Wait for connections on the ServerSocket, accepts, create a new (client)
+     * socket for each and a call the abstract method createNewConnection with
+     * each new socket.
+     */
     @Override
     public void run() {
         try {
@@ -41,6 +56,11 @@ public abstract class AbstractListener extends Thread {
         }
     }
 
+    /**
+     * Called each time a new connection is accepted.
+     * @param sock The (client) Socket created for this connection.
+     * @throws IOException
+     */
     protected abstract void createNewConnection(Socket sock) throws IOException;
 
 }
